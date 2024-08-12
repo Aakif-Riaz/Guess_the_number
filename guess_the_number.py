9fda2ea61670ca9f9d57c02907ab00f618a7c36c
Aakif Riaz <119338145+Aakif-Riaz@users.noreply.github.com>
2024-08-12 23:18:24 +0500

    Add files via upload
    A simple Number Guessing Game built with Python.

---
+++ guess_the_number.py
@@ -0,0 +1,7 @@
+number = int(input("Guess the Number:"))
+list_numbers = [45]
+for integer in list_numbers:
+    if integer == 45 and number == 45:
+        print("Correct Guess")
+    else:
+        print("Try Again")
